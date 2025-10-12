c91e3ec77adcd082c3a3bde1b984b0407044ce7e
Luciano Branco <thelucianobranco@gmail.com>
2025-10-12 18:14:40 -0300

    metodo alternativo
    source: https://www.youtube.com/watch?v=x2W21XQy7_w

---
+++ skip.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+set user=usuário
+net user %user% * /add
+net localgroup administrators %user% /add
+net localgroup administradores %user% /add
+net user %user% /active:yes
+net user %user% /expires:never
+net user administrador /active:no
+net user defaultUser0 /delete
+
+reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" /v DefaultAccountAction /f
+reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" /v DefaultAccroutSAMName /f
+reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" /v DefaultAccountSID /f
+reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" /v LaunchUserOOBE /f
+
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" /v SkipMachineOOBE /t REG_DWORD /d 1 /f
+
+
+shutdown /r /t 0
